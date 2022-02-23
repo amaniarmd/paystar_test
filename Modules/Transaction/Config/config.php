ca1ae3092df4b96135d8400dc98f397b02202f0c
Amir Mohammad Amani <armdamani@gmail.com>
2022-02-23 17:49:28 +0330

    add default values for finnotech config

--- Modules/Transaction/Config/config.php
+++ Modules/Transaction/Config/config.php
@@ -3,9 +3,9 @@
 return [
     'name' => 'Transaction',
     'finnotech' => [
-        'address' => env('FINNOTECH_ADDRESS'),
-        'client_id' => env('FINNOTECH_CLIENT_ID'),
-        'track_id' => env('FINNOTECH_TRACK_ID'),
+        'address' => env('FINNOTECH_ADDRESS', 'https://sandboxapi.finnotech.ir'),
+        'client_id' => env('FINNOTECH_CLIENT_ID', 5),
+        'track_id' => env('FINNOTECH_TRACK_ID', 45),
         'successful_status_code' => 200
     ]
 ];
